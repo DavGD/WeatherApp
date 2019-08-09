b4d4b1f1b76c57d44e941a77970e4e0ac4553f1f
DavGD <mrdawid95@gmail.com>
2019-08-09 17:06:02 +0200

    form pass to another view, simple change css

--- weather/models.py
+++ weather/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class CityField(models.Model):
     city = models.CharField(max_length = 25)
+
+    def __str__(self):
+        return self.city
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 import requests
 from .forms import CityForm
+from .models import CityField
 
 
 def main_site(request):
@@ -15,18 +16,23 @@ def main_site(request):
 
 def city_site(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=8cc2f0a4630ad36fc89c8ab644ef1e28'
-    city = 'Konin'
 
-    r = requests.get(url.format(city)).json()
+    try:
+        city =  CityField.objects.all().last()
 
-    city_weather = {
-        'country' : r['sys']['country'],
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'weather' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
-    }
+        r = requests.get(url.format(city)).json()
 
-    context = {'city_weather' : city_weather}
+        city_weather = {
+            'country' : r['sys']['country'],
+            'city' : r['name'],
+            'temperature' : r['main']['temp'],
+            'weather' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
 
-    return render(request, 'weather/details.html', context)
+        context = {'city_weather' : city_weather}
+
+        return render(request, 'weather/details.html', context)
+
+    except KeyError:
+        return redirect('main_site')
