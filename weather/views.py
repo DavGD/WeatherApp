2e12d4ca2c658e8fa1126b3f3abb62e2936fdd59
DavGD <mrdawid95@gmail.com>
2019-08-30 14:48:12 +0200

    change css, html and views

--- weather/views.py
+++ weather/views.py
@@ -11,11 +11,21 @@ def main_site(request):
             form.save()
             return redirect('city_site')
     else:
-        form = CityForm()
-        return render(request, 'weather/index.html', {'form':form})
+        lastest = CityField.objects.values_list('city', flat=True).order_by('-id')[:10]
+        last_search = []
+        for i in lastest:
+            if i not in last_search:
+                last_search.append(i)
+
+        con_site = {
+            'form' : CityForm(),
+            'last_five' : last_search[:5],
+        }
+
+        return render(request, 'weather/main.html', {'con_site': con_site})
 
 def city_site(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=8cc2f0a4630ad36fc89c8ab644ef1e28'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=8cc2f0a4630ad36fc89c8ab644ef1e28'
 
     try:
         city =  CityField.objects.all().last()
@@ -25,7 +35,7 @@ def city_site(request):
         city_weather = {
             'country' : r['sys']['country'],
             'city' : r['name'],
-            'temperature' : r['main']['temp'],
+            'temperature' : int(r['main']['temp']),
             'weather' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
         }
